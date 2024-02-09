ac45b237bcc5134620e637a7dff497ecfeb94a28
michał <michal.adamczewski00@gmail.com>
2024-02-09 21:39:58 +0100

    mongoDB

---
+++ db/mongoImporter.py
@@ -0,0 +1,17 @@
+import pymongo
+import gridfs
+import json
+
+client = pymongo.MongoClient('mongodb://admin:admin@localhost:27017/?authMechanism=DEFAULT')
+db = client.get_database('app_shop')
+products = db.get_collection('products')
+fs = gridfs.GridFS(db)
+
+with open('data.json', encoding='utf-8') as f:
+    data = json.load(f, )
+    for product in data:
+        path = '../' + product['imgPath']
+        del product['imgPath']
+        id = products.insert_one(product).inserted_id
+        with open(path, 'rb') as image:
+            fs.put(image, product_id=id, file_type='png')
